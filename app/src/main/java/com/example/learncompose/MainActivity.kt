59aee6a176c63d659418ec7b406c040df3e482d7
Adithya-Egc <adithyarang@gmail.com>
2023-10-07 16:15:53 +0530

    Initial Project

---
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.learncompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainComponent()
+        }
+    }
+}
+
+@Composable
+fun MainComponent() {
+
+
+    var count = rememberSaveable() {
+        mutableStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = { count.value++ }) {
+                Text(text = "Click Me.${count.value}")
+            }
+
+        }
+    }
+}
